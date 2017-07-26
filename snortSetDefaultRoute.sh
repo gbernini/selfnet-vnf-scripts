f7fa18e5e120a55bb90a7ed59d723afc1bf80e96
Giacomo Bernini <g.bernini@nextworks.it>
2017-07-26 16:05:41 +0200

    upd

--- snortSetDefaultRoute.sh
+++ snortSetDefaultRoute.sh
@@ -29,8 +29,8 @@ MGMT_NET_GW=$FIRST.$SECOND.$THIRD.1
 echo "Setting route for Kafka bus and github to gateway: $MGMT_NET_GW"
 
 ip route add 10.255.255.129/32 via $MGMT_NET_GW
-ip route add 192.30.252.0/22 via $MGMT_NET_GW
-ip route add 185.199.108.0/22 via $MGMT_NET_GW
+#ip route add 192.30.252.0/22 via $MGMT_NET_GW
+#ip route add 185.199.108.0/22 via $MGMT_NET_GW
 
 echo "Done."
 

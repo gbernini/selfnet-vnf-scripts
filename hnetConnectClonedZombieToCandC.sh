d890a17b18774c07fe0e184bd2e03c80dbc527e9
Giacomo Bernini <g.bernini@nextworks.it>
2017-08-23 12:24:30 +0200

    upd

--- hnetConnectClonedZombieToCandC.sh
+++ hnetConnectClonedZombieToCandC.sh
@@ -1,51 +1,64 @@
 #!/bin/bash
 
-if [ -z ${TYPE} ]; then
-	(>&2 echo "Missing TYPE parameter")
-	exit 1
+if [ -z ${CID} ]; then
+        (>&2 echo "Missing CID parameter")
+        exit 1
 fi
 
-if [ -z ${FREQ} ]; then
-	(>&2 echo "Missing FREQ parameter")
-	exit 1
-fi
+#check if it is not DELETE operation
+if [ -z ${DELETE} ]; then
 
-if [ -z ${BOT} ]; then
-	(>&2 echo "Missing BOT parameter")
-	exit 1
-fi
+	if [ -z ${TYPE} ]; then
+		(>&2 echo "Missing TYPE parameter")
+		exit 1
+	fi
 
-if [ -z ${VERBOSE} ]; then
-	VERBOSE=true
-fi
+	if [ -z ${FREQ} ]; then
+		(>&2 echo "Missing FREQ parameter")
+		exit 1
+	fi
 
-if [ -z ${CCIP} ]; then
- 	(>&2 echo "Missing CCIP parameter")
-	exit 1
-fi
+	if [ -z ${BOT} ]; then
+		(>&2 echo "Missing BOT parameter")
+		exit 1
+	fi
 
-if [ -z ${ZOMBIEUID} ]; then
- 	(>&2 echo "Missing ZOMBIEUID parameter")
-	exit 1
-fi
+	if [ -z ${VERBOSE} ]; then
+		VERBOSE=true
+	fi
 
-if [ -z ${LOCALPORT} ]; then
-        LOCALPORT=0
-fi
+	if [ -z ${CCIP} ]; then
+ 		(>&2 echo "Missing CCIP parameter")
+		exit 1
+	fi
 
-if [ -z ${CCSUB} ]; then
-	echo "C&C server subnet not provided."
+	if [ -z ${ZOMBIEUID} ]; then
+ 		(>&2 echo "Missing ZOMBIEUID parameter")
+		exit 1
+	fi
+
+	if [ -z ${LOCALPORT} ]; then
+        	LOCALPORT=0
+	fi
+
+	if [ -z ${CCSUB} ]; then
+		echo "C&C server subnet not provided."
+	else
+		echo "C&C server subnet ${CCSUB} not needed. HNet VNF is running with a single vNIC"
+	fi
+
+	java -jar /home/nextworks/ZombieBot.jar -type ${TYPE} -freq ${FREQ} -bot ${BOT} -uid ${ZOMBIEUID} -v ${VERBOSE} ${CCIP} &
+
+	sleep 1
+
+	zombie=`pgrep -f "ZombieBot.jar -type ${TYPE} -freq ${FREQ} -bot ${BOT} -uid ${ZOMBIEUID} -port ${LOCALPORT} -v ${VERBOSE} ${CCIP}"`
+
+	if [ -z ${zombie} ]; then
+ 		(>&2 echo "HNet app configuration failure.")
+		exit 1
+	fi
+
 else
-	echo "C&C server subnet ${CCSUB} not needed. HNet VNF is running with a single vNIC"
-fi
-
-java -jar /home/nextworks/ZombieBot.jar -type ${TYPE} -freq ${FREQ} -bot ${BOT} -uid ${ZOMBIEUID} -v ${VERBOSE} ${CCIP} &
-
-sleep 1
-
-zombie=`pgrep -f "ZombieBot.jar -type ${TYPE} -freq ${FREQ} -bot ${BOT} -uid ${ZOMBIEUID} -port ${LOCALPORT} -v ${VERBOSE} ${CCIP}"`
-
-if [ -z ${zombie} ]; then
- 	(>&2 echo "HNet app configuration failure.")
-	exit 1
+	#it is DELETE operation
+	echo "HNet app configuration delete not implemented !!!"
 fi
--- snortConfigureRule.sh
+++ snortConfigureRule.sh
@@ -1,36 +1,49 @@
 #!/bin/bash
 
-if [ -z ${ZOMBIEIP} ]; then
-	(>&2 echo "Missing ZOMBIEIP parameter")
-	exit 1
-fi
-
-if [ -z ${CCIP} ]; then
-        (>&2 echo "Missing CCIP parameter")
+if [ -z ${CID} ]; then
+        (>&2 echo "Missing CID parameter")
         exit 1
 fi
 
-if [ -z ${CCPORT} ]; then
-        (>&2 echo "Missing CCPORT parameter")
-        exit 1
+#check if it is not DELETE operation
+if [ -z ${DELETE} ]; then
+	if [ -z ${ZOMBIEIP} ]; then
+        	(>&2 echo "Missing ZOMBIEIP parameter")
+        	exit 1
+	fi
+
+	if [ -z ${CCIP} ]; then
+        	(>&2 echo "Missing CCIP parameter")
+        	exit 1
+	fi
+
+	if [ -z ${CCPORT} ]; then
+        	(>&2 echo "Missing CCPORT parameter")
+        	exit 1
+	fi
+
+	if [ -z ${MESSAGE} ]; then
+        	(>&2 echo "Missing MESSAGE parameter")
+        	exit 1
+	fi
+
+	if [ -z ${PARAMS} ]; then
+        	(>&2 echo "Missing PARAMS parameter")
+        	exit 1
+	fi
+
+	rule=`echo "alert tcp ${ZOMBIEIP} any -> ${CCIP} ${CCPORT} (msg:\"${MESSAGE}\";content:\"GET\"; content:\"${PARAMS}\"; nocase; sid:${CID};)"`
+
+	echo "Adding new signature to Snort:"
+	echo $rule
+
+	echo $rule >> /etc/snort/rules/selfnet.rules
+else
+	#it is delete operation
+	echo "Removing signature with sid ${CID} to Snort:"
+	sed -i "/sid:${CID};/d" /etc/snort/rules/selfnet.rules
+
 fi
-
-if [ -z ${MESSAGE} ]; then
-        (>&2 echo "Missing MESSAGE parameter")
-        exit 1
-fi
-
-if [ -z ${PARAMS} ]; then
-        (>&2 echo "Missing PARAMS parameter")
-        exit 1
-fi
-
-rule=`echo "alert tcp ${ZOMBIEIP} any -> ${CCIP} ${CCPORT} (msg:\"${MESSAGE}\";content:\"GET\"; content:\"${PARAMS}\"; nocase; sid:10001;)"`
-
-echo "Adding new signature to Snort:"
-echo $rule
-
-echo $rule >> /etc/snort/rules/selfnet.rules
 
 service snort restart
 

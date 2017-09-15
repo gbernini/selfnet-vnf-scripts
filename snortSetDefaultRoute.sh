869c7002ef6702cbcdbf3a9951418d1482238e3d
Giacomo Bernini <g.bernini@nextworks.it>
2017-09-15 17:35:55 +0200

    fix

--- snortSetDefaultRoute.sh
+++ snortSetDefaultRoute.sh
@@ -7,6 +7,28 @@ THIRD_S=$(echo $ap_provider_net| cut -d'.' -f 3)
 
 GW_DFLT=$FIRST_S.$SECOND_S.$THIRD_S.1
 
+##check if /etc/network/interfaces has been already tweaked
+if grep -Fxq "##DONE##" test.txt
+then
+	#do nothing
+else
+	#set ip for ens4 -> ap_provider_net
+	echo "Setting IP to ens4 interface: $ap_provider_net"
+
+	sed -i "s/auto ens4/#auto ens4/g" /etc/network/interfaces
+
+	echo "auto ens4" >> /etc/network/interfaces
+	echo "iface ens4 inet static" >> /etc/network/interfaces
+	echo "address ${ap_provider_net}" >> /etc/network/interfaces
+	echo "netmask 255.255.255.0" >> /etc/network/interfaces
+	echo "gateway ${GW_DFLT}" >> /etc/network/interfaces
+	echo "##DONE##" >> /etc/network/interfaces
+	ifdown ens4
+	ifup ens4
+fi
+
+
+
 #set ip for ens4 -> ap_provider_net
 echo "Setting IP to ens4 interface: $ap_provider_net"
 

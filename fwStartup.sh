b02ca439bb2efd4ba588fa6f6b8eae8d0a9fb8f1
Giacomo Bernini <g.bernini@nextworks.it>
2018-02-14 16:31:08 +0100

    upd

--- fwStartup.sh
+++ fwStartup.sh
@@ -7,15 +7,14 @@ sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost $name/g" /etc/hosts
 IP_DOWN="$fw_downstream/24"
 IP_UP="$fw_upstream/24"
 
-export FW_IP_DOWNSTREAM="$IP_DOWN"
-export FW_IP_UPSTREAM="$IP_UP"
-export FLOWT_MAC_UPSTREAM='fa:be:af:d3:cb:b8'
-export FLOWT_MAC_DOWNSTREAM='fa:be:af:a2:6c:ba'
-export FLOWT_IP_UPSTREAM='172.12.0.7'
-export FLOWT_IP_DOWNSTREAM='172.13.0.5'
+sed -i '2 i\FW_IP_DOWNSTREAM='"$IP_DOWN"'' /opt/firewall_server/init_config/config_ovs.sh
+sed -i '2 i\FW_IP_UPSTREAM='"$IP_UP"'' /opt/firewall_server/init_config/config_ovs.sh
+sed -i '2 i\FLOWT_MAC_UPSTREAM=''fa:be:af:d3:cb:b8''' /opt/firewall_server/init_config/config_ovs.sh
+sed -i '2 i\FLOWT_MAC_DOWNSTREAM=''fa:be:af:a2:6c:ba''' /opt/firewall_server/init_config/config_ovs.sh
+sed -i '2 i\FLOWT_IP_UPSTREAM=''172.12.0.7''' /opt/firewall_server/init_config/config_ovs.sh
+sed -i '2 i\FLOWT_IP_DOWNSTREAM=''172.13.0.5''' /opt/firewall_server/init_config/config_ovs.sh
 
 systemctl start firewall_server_ovs_init.service
 systemctl start firewall_server.service
 
 echo "FW VNF properly started."
-

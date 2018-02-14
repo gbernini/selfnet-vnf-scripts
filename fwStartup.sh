3f62f3ba9da87d574fd787009b53d3af7af978ff
Giacomo Bernini <g.bernini@nextworks.it>
2018-02-14 12:07:52 +0100

    upd fw scripts

--- fwStartup.sh
+++ fwStartup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-export FLOWT_UPSTREAM='fa:be:af:d3:cb:b8'
-export FLOWT_DOWNSTREAM='fa:be:af:a2:6c:ba'
-
-ping -c 1 10.0.255.1 > ping.out
-
 name=`echo "$hostname" | awk '{print tolower($0)}'`
 
 sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost $name/g" /etc/hosts
+
+IP_DOWN="$fw_downstream/24"
+IP_UP="$fw_upstream/24"
+
+export FW_IP_DOWNSTREAM="$IP_DOWN"
+export FW_IP_UPSTREAM="$IP_UP"
+export FLOWT_MAC_UPSTREAM='fa:be:af:d3:cb:b8'
+export FLOWT_MAC_DOWNSTREAM='fa:be:af:a2:6c:ba'
+export FLOWT_IP_UPSTREAM='172.12.0.7'
+export FLOWT_IP_DOWNSTREAM='172.13.0.5'
+
+systemctl start firewall_server_ovs_init.service
+systemctl start firewall_server.service
 
 echo "FW VNF properly started."
 

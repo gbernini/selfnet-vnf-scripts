14c7503dc57c41ec359dfd323c199d711d38785d
Giacomo Bernini <g.bernini@nextworks.it>
2018-02-14 18:39:05 +0100

    upd

--- fwStartup.sh
+++ fwStartup.sh
@@ -9,10 +9,10 @@ IP_UP="$fw_upstream/24"
 
 sed -i '2 i\FW_IP_DOWNSTREAM='"$IP_DOWN"'' /opt/firewall_server/init_config/config_ovs.sh
 sed -i '2 i\FW_IP_UPSTREAM='"$IP_UP"'' /opt/firewall_server/init_config/config_ovs.sh
-sed -i '2 i\FLOWT_MAC_UPSTREAM=''fa:be:af:d3:cb:b8''' /opt/firewall_server/init_config/config_ovs.sh
-sed -i '2 i\FLOWT_MAC_DOWNSTREAM=''fa:be:af:a2:6c:ba''' /opt/firewall_server/init_config/config_ovs.sh
-sed -i '2 i\FLOWT_IP_UPSTREAM=''172.12.0.7''' /opt/firewall_server/init_config/config_ovs.sh
-sed -i '2 i\FLOWT_IP_DOWNSTREAM=''172.13.0.5''' /opt/firewall_server/init_config/config_ovs.sh
+sed -i '2 i\FLOWT_MAC_UPSTREAM=''"'fa:be:af:d3:cb:b8'"''' /opt/firewall_server/init_config/config_ovs.sh
+sed -i '2 i\FLOWT_MAC_DOWNSTREAM=''"'fa:be:af:a2:6c:ba'"''' /opt/firewall_server/init_config/config_ovs.sh
+sed -i '2 i\FLOWT_IP_UPSTREAM=''"'172.12.0.7'"''' /opt/firewall_server/init_config/config_ovs.sh
+sed -i '2 i\FLOWT_IP_DOWNSTREAM=''"'172.13.0.5'"''' /opt/firewall_server/init_config/config_ovs.sh
 
 systemctl start firewall_server_ovs_init.service
 systemctl start firewall_server.service

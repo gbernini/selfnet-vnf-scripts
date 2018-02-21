10ae254081fdc834e7936c3f897a3fa22d3e8006
Giacomo Bernini <g.bernini@nextworks.it>
2018-02-21 13:12:19 +0100

    added blea

---
+++ bleaStartup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+name=`echo "$hostname" | awk '{print tolower($0)}'`
+
+sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost $name/g" /etc/hosts
+
+#set hostname for kafka bus VM
+echo "10.255.255.151 selfnetmont-dev-evl-kafka.ptin.corppt.com" >> /etc/hosts
+
+#set hostname for Kafka bus VM into kafka publishing app configuration
+echo -n "selfnetmont-dev-evl-kafka.ptin.corppt.com" >> /home/nextworks/kafkaServer.cfg
+
+ping -c 1 -I $interface 10.0.255.1 >> ping.out
+
+sudo python3 ~/url_sniffer/thread_url_sniffer.py --iface ens4 >> blea.log &
+
+echo "BLEA VNF properly started."
